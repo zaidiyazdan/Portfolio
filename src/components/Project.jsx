d6779653f4ccd87019c4e4ce11c9ac66de34d00a
zaidiyazdan <117353242+zaidiyazdan@users.noreply.github.com>
2024-05-10 13:41:15 +0530

    UI improveemnts

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,7 @@ import drivaMate from "../assets/driveMate2.png"
 
 const Project = () => {
   return (
-    <div className="bg-gradient-to-b flex flex-col justify-start items-center lg:px-60 py-6 to-blue-950 from-black ">
+    <div id="projects" className="bg-gradient-to-b flex flex-col justify-start items-center lg:px-60 py-6 to-blue-950 from-black ">
       <h1 className="text-4xl sm:text-5xl font-kanit  font-semibold bg-clip-text text-transparent bg-gradient-to-b from-blue-500 to-blue-700">My Projects</h1>
       <div className="text-center  flex justify-center flex-col gap-3">
         <h1 className="text-3xl md:hidden py-8 px-4  font-kanit italic font-normal text-white text-shadow-black">
